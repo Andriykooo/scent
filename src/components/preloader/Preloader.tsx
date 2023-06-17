cd066389d33148060be0daec20473e6502cff1fc
Andrii <blackdog337.ab@gmail.com>
2023-06-17 17:45:28 +0300

    created preloader

---
+++ src/components/preloader/Preloader.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import styles from "./preloader.module.scss";
+
+const animationDuration = 2100;
+
+export const Preloader: React.FC = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  const disablePreloader = () => {
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(disablePreloader, animationDuration);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    isActive && (
+      <div className={styles.preloader}>
+        <Image
+          src="/animated-logo.gif"
+          className={styles.preloader_logo}
+          height={100}
+          width={300}
+          alt="logo"
+        />
+      </div>
+    )
+  );
+};
