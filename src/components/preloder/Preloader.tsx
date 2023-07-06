4c4baee902d1984d05a9c9b3bbcd2026681b0a78
Andrii <blackdog337.ab@gmail.com>
2023-07-06 22:37:18 +0300

    preloader fix

--- src/components/preloder/Preloader.tsx
+++ src/components/preloder/Preloader.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useEffect, useState } from "react";
 import { Logo } from "../logo/Logo";
 import styles from "./preloader.module.scss";
 
 export const Preloader: FC = () => {
+  const [animationSinished, setAnimationSinished] = useState(true);
+
+  const finishAnimation = () => {
+    setAnimationSinished(false);
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(finishAnimation, 2000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-    <div className={styles.preloader}>
-      <Logo className={styles.preloader_logo} />
-    </div>
+    animationSinished && (
+      <div className={styles.preloader}>
+        <Logo className={styles.preloader_logo} />
+      </div>
+    )
   );
 };
