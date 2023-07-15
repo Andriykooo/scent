cdbee9ac87617e7d548cf2e5005f88722fbc8335
Andrii <blackdog337.ab@gmail.com>
2023-07-15 14:32:59 +0300

    added left side

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,47 @@
 import { FC } from "react";
 import styles from "./footer.module.scss";
+import { Logo } from "../logo/Logo";
+import { variants } from "@/constants/variants";
+import Link from "next/link";
 
-export const Footer:FC = () => {
-  return <footer className={styles.footer}>footer</footer>;
+const footerList = [
+  {
+    id: 1,
+    name: "About us",
+    link: "/about-us",
+  },
+  {
+    id: 2,
+    name: "Terms of use",
+    link: "/terms-of-use",
+  },
+  {
+    id: 3,
+    name: "Payment methods",
+    link: "/payment-methods",
+  },
+];
+
+export const Footer: FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer_content}>
+        <div className={styles.footer_content_left}>
+          <Logo variant={variants.SECOND} />
+          <ul className={styles.footer_list}>
+            {footerList.map((item) => {
+              return (
+                <li key={item.id}>
+                  <Link href={item.link}> {item.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className={styles.footer_content_right}>
+          <Logo variant={variants.SECOND} />
+        </div>
+      </div>
+    </footer>
+  );
 };
