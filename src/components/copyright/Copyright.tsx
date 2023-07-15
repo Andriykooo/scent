04d04c1dcb8d0633d63e39115196be6d2a19593f
Andrii <blackdog337.ab@gmail.com>
2023-07-15 17:09:36 +0300

    updated copyright

--- src/components/copyright/Copyright.tsx
+++ src/components/copyright/Copyright.tsx
@@ -2,5 +2,11 @@ import { FC } from "react";
 import styles from "./copyright.module.scss";
 
 export const Copyright: FC = () => {
-  return <span className={styles.copyright}>©2023 Scent Marketplace</span>;
+  const year = new Date().getFullYear();
+
+  return (
+    <span className={styles.copyright}>
+      Copyright © {year} | Scent Marketplace
+    </span>
+  );
 };
