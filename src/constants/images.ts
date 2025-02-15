695b1665b1e526cf2e25c3784e06dd44aa4de328
Andriykooo <blackdog337.ab@gmail.com>
2025-02-15 16:58:17 +0200

    qwe

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,7 @@
 export const bannerImages = {
   IMAGE_1: "/images/banner/Group_4470.png",
   IMAGE_2: "/images/banner/Group_4472.png",
-  IMAGE_3: "/images/banner/banner_3.jpg",
+  IMAGE_3: "/images/banner/video.gif",
   IMAGE_4: "/images/banner/banner_4.jpg",
   IMAGE_5: "/images/banner/pxfuel _1.png",
   BLOG_IMAGE_1: "/images/banner/imageUserBlog1.png",
--- src/mock/banner.ts
+++ src/mock/banner.ts
@@ -3,6 +3,11 @@ import { BannerType } from "@/types/bannerType";
 
 export const bannerData: BannerType[] = [
   {
+    id: 0,
+    title: "",
+    image: bannerImages.IMAGE_3,
+  },
+  {
     id: 1,
     title: "",
     image: bannerImages.IMAGE_1,
